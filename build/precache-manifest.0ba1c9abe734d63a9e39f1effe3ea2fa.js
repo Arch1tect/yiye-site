1eabc95b00481812d4a6102ed1c699f2e7c8ccb3
ArchiTech <swtdavid@gmail.com>
2020-03-28 14:50:53 -0700

    6.0

---
+++ build/precache-manifest.0ba1c9abe734d63a9e39f1effe3ea2fa.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "7969ce4eede8be9ae7e7f30999b42237",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d65053e265e48195a4e8",
+    "url": "./content-static/js/main.js"
+  },
+  {
+    "revision": "d65053e265e48195a4e8",
+    "url": "./content-static/css/main.css"
+  }
+];
